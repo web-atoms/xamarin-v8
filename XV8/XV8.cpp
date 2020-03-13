cae30dd976c6085e42beb42cd375b8eda16c0fe3
Akash Kava <ackava@gmail.com>
2020-03-13 18:09:45 +0530

    Added support for handles

--- XV8/XV8.cpp
+++ XV8/XV8.cpp
@@ -1,25 +1,77 @@
 #include "XV8.h"
+#include "libplatform.h"
 
+using namespace v8;
 static bool _V8Initialized = false;
 
 V8Context* V8Context_Create() {
 	return new V8Context();
 }
 
+void V8Context_Dispose(V8Context* context) {
+	context->Dispose();
+	delete context;
+}
+
+V8Handle<Value> V8Context_CreateNumber(V8Context* context, double value) {
+	return context->CreateNumber(value);
+}
+
+template<class T>
+V8Handle<T> ToHandle(Local<T> value) {
+	V8Handle<T> h;
+	h.handle.Reset(value);
+	reutrn h;
+}
+
+template<class T>
+void DisposeHandle(V8Handle<T> handle) {
+	handle.handle.Reset();
+}
+
+
 V8Context::V8Context() {
 	if (!_V8Initialized) // (the API changed: https://groups.google.com/forum/#!topic/v8-users/wjMwflJkfso)
 	{
-		v8::V8::InitializeICU();
+		V8::InitializeICU();
 
 		//?v8::V8::InitializeExternalStartupData(PLATFORM_TARGET "\\");
 		// (Startup data is not included by default anymore)
 
-		_Platform = v8::Platform::
-		v8::V8::InitializePlatform(_Platform.get());
+		_platform = platform::NewDefaultPlatform();
+		V8::InitializePlatform(_platform.get());
 
-		v8::V8::Initialize();
+		V8::Initialize();
 
 		_V8Initialized = true;
 	}
+	
+	Isolate::CreateParams params;
+	_arrayBufferAllocator = ArrayBuffer::Allocator::NewDefaultAllocator();
+	params.array_buffer_allocator = _arrayBufferAllocator;
+	
+	_isolate = Isolate::New(params);
+	Local<Object> global = Object::New(_isolate);
+	Local<Context> c = Context::New(_isolate, nullptr, global);
+	_context.Reset(_isolate, c);
+	
 
+}
+
+void V8Context::Dispose() {
+
+	_context.Reset();
+
+	_isolate->Dispose();
+	// delete _Isolate;
+	delete _arrayBufferAllocator;
+
+}
+
+V8Handle<Object> V8Context::CreateObject() {
+	return ToHandle(Object::New(_isolate));
+}
+
+V8Handle<Number> V8Context::CreateNumber(double value) {
+	return ToHandle(Number::New(_isolate));
 }
--- XV8/XV8.h
+++ XV8/XV8.h
@@ -2,9 +2,44 @@
 #include "v8.h"
 using namespace v8;
 
+#pragma comment(lib, "libicudata.a");
+#pragma comment(lib, "libv8_base.a");
+#pragma comment(lib, "libv8_init.a");
+#pragma comment(lib, "libv8_initializers.a");
+#pragma comment(lib, "libv8_libbase.a");
+#pragma comment(lib, "libv8_libplatform.a");
+#pragma comment(lib, "libv8_libsampler.a");
+#pragma comment(lib, "libv8_nosnapshot.a");
+#pragma comment(lib, "libv8_snapshot.a");
+
+
+template<class T>
+struct V8Handle {
+	Persistent<T, CopyablePersistentTraits<T>> handle;
+};
+
+template<class T>
+V8Handle<T> ToHandle(Local<T> value);
+
+template<class T>
+void DisposeHandle(V8Handle<T> handle);
+
+
 class V8Context {
 protected:
 	int32_t _engineID;
 
-	std::unique_ptr<v8::Platform> _Platform;
+	std::unique_ptr<Platform> _platform;
+	Isolate* _isolate;
+	Persistent<Context, CopyablePersistentTraits<Context>> _context;
+	EscapableHandleScope* _scope;
+
+	// delete array allocator
+	ArrayBuffer::Allocator* _arrayBufferAllocator;
+public:
+	V8Context();
+	void Dispose();
+
+	V8Handle<Object> CreateObject();
+	V8Handle<Number> CreateNumber(double value);
 };
