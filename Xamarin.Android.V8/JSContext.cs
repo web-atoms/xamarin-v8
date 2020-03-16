9311bbddea29876b7ee4b87254e165697fc19a74
Akash Kava <ackava@gmail.com>
2020-03-16 12:15:09 +0530

    Dll load issue not fixed

--- Xamarin.Android.V8/JSContext.cs
+++ Xamarin.Android.V8/JSContext.cs
@@ -11,6 +11,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 
+
 namespace Xamarin.Android.V8
 {
 
@@ -20,6 +21,8 @@ namespace Xamarin.Android.V8
 
     public class JSContext
     {
+
+        const string LibName = "XV8";
 
         SafeHandle context;
         public JSContext()
@@ -80,46 +83,46 @@ namespace Xamarin.Android.V8
             V8Context_Dispose(context);
         }
 
-        [DllImport("libXV8")]
+        [DllImport(LibName)]
         internal extern static SafeHandle V8Context_Create();
 
-        [DllImport("libXV8")]
+        [DllImport(LibName)]
         internal extern static void V8Context_Dispose(SafeHandle context);
 
-        [DllImport("libXV8")]
+        [DllImport(LibName)]
         internal extern static V8Response V8Context_CreateObject(SafeHandle context);
 
 
-        [DllImport("libXV8")]
+        [DllImport(LibName)]
         internal extern static V8Response V8Context_CreateNull(SafeHandle context);
 
-        [DllImport("libXV8")]
+        [DllImport(LibName)]
         internal extern static V8Response V8Context_CreateUndefined(SafeHandle context);
 
-        [DllImport("libXV8")]
+        [DllImport(LibName)]
         internal extern static V8Response V8Context_CreateArray(SafeHandle context);
 
-        [DllImport("libXV8")]
+        [DllImport(LibName)]
         internal extern static V8Response V8Context_CreateBoolean(SafeHandle context, bool value);
 
-        [DllImport("libXV8")]
+        [DllImport(LibName)]
         internal extern static V8Response V8Context_CreateNumber(SafeHandle context, double value);
 
-        [DllImport("libXV8")]
+        [DllImport(LibName)]
         internal extern static V8Response V8Context_CreateDate(SafeHandle context, long value);
 
-        [DllImport("libXV8")]
+        [DllImport(LibName)]
         internal extern static V8Response V8Context_CreateString(SafeHandle context, 
             [MarshalAs(UnmanagedType.LPUTF8Str)] string value);
 
-        [DllImport("libXV8")]
+        [DllImport(LibName)]
         internal extern static V8Response V8Context_GetProperty(
             SafeHandle context, 
             IntPtr handle, 
             [MarshalAs(UnmanagedType.LPUTF8Str)]
             string name);
 
-        [DllImport("libXV8")]
+        [DllImport(LibName)]
         internal extern static V8Response V8Context_SetProperty(
             SafeHandle context,
             IntPtr handle,
@@ -127,29 +130,29 @@ namespace Xamarin.Android.V8
             string name,
             IntPtr value);
 
-        [DllImport("libXV8")]
+        [DllImport(LibName)]
         internal extern static V8Response V8Context_GetPropertyAt(SafeHandle context, IntPtr handle, int index);
 
 
-        [DllImport("libXV8")]
+        [DllImport(LibName)]
         internal extern static V8Response V8Context_SetPropertyAt(
             SafeHandle context,
             IntPtr handle,
             int index,
             IntPtr value);
 
-        [DllImport("libXV8")]
+        [DllImport(LibName)]
         internal extern static void V8Context_Release(V8Response r);
 
 
-        [DllImport("libXV8")]
+        [DllImport(LibName)]
         internal extern static void V8Context_ReleaseHandle(IntPtr r);
 
-        [DllImport("libXV8")]
+        [DllImport(LibName)]
         internal extern static V8Response V8Context_CreateFunction(
             SafeHandle context, IntPtr function, [MarshalAs(UnmanagedType.LPUTF8Str)]string name);
 
-        [DllImport("libXV8")]
+        [DllImport(LibName)]
         internal extern static V8Response V8Context_Evaluate(
             SafeHandle context,
             [MarshalAs(UnmanagedType.LPUTF8Str)]
