3bb86fc4e94bc886b0f21b11517258b67ca79b0e
Akash Kava <39438041+ackava@users.noreply.github.com>
2020-03-25 12:02:32 +0530

    Trying simple android build

--- scripts/env.sh
+++ scripts/env.sh
@@ -2,48 +2,23 @@
 
 function abs_path()
 {
-  readlink="readlink -f"
-  if [[ "$(uname)" == "Darwin" ]]; then
-    if [[ ! "$(command -v greadlink)" ]]; then
-      echo "greadlink not found. Please install greadlink by \`brew install coreutils\`" >&2
-      exit 1
+    readlink="readlink -f"
+    if [[ "$(uname)" == "Darwin" ]]; then
+        if [[ ! "$(command -v greadlink)" ]]; then
+            echo "greadlink not found. Please install greadlink by \`brew install coreutils\`"
+            exit 1
+        fi
+      readlink="greadlink -f"
     fi
-    readlink="greadlink -f"
-  fi
 
-  echo `$readlink $1`
-}
-
-function verify_platform()
-{
-  local arg=$1
-  SUPPORTED_PLATFORMS=(android ios)
-  local valid_platform=
-  for platform in ${SUPPORTED_PLATFORMS[@]}
-  do
-    if [[ ${arg} = ${platform} ]]; then
-      valid_platform=${platform}
-    fi
-  done
-  if [[ -z ${valid_platform} ]]; then
-    echo "Invalid platfrom: ${arg}" >&2
-    exit 1
-  fi
-  echo ${valid_platform}
+    echo `$readlink $1`
 }
 
 CURR_DIR=$(dirname $(abs_path $0))
-ROOT_DIR=$(dirname ${CURR_DIR})
+ROOT_DIR=$(dirname $CURR_DIR)
 unset CURR_DIR
 
-DEPOT_TOOLS_DIR="${ROOT_DIR}/scripts/depot_tools"
-BUILD_DIR="${ROOT_DIR}/build"
-V8_DIR="${ROOT_DIR}/v8"
-DIST_DIR="${ROOT_DIR}/dist"
-PATCHES_DIR="${ROOT_DIR}/patches"
-
-NDK_VERSION="r19c"
-IOS_DEPLOYMENT_TARGET="9"
-
-export PATH="$DEPOT_TOOLS_DIR:$PATH"
-PLATFORM=$(verify_platform $1)
+BUILD_DIR=$ROOT_DIR/build
+DIST_DIR=$ROOT_DIR/dist
+SRC_DIR=$ROOT_DIR/src
+PATCHES_DIR=$SRC_DIR/patches
--- scripts/export_npm_env.sh
+++ scripts/export_npm_env.sh
@@ -1,11 +1,6 @@
 #!/bin/bash -e
 if [[ ${CIRCLECI} ]]; then
-  echo "export VERSION=${npm_package_version}" >> $BASH_ENV
-  echo "export V8_VERSION=${npm_package_config_V8}" >> $BASH_ENV
-elif [[ ${GITHUB_ACTIONS} ]]; then
-  echo "::set-env name=VERSION::${npm_package_version}"
-  echo "::set-env name=V8_VERSION::${npm_package_config_V8}"
+    echo "export RN_VERSION=${npm_package_config_RN_VERSION}" >> $BASH_ENV
 else
-  export VERSION=${npm_package_version}
-  export V8_VERSION=${npm_package_config_V8}
+    export RN_VERSION=${npm_package_config_RN_VERSION}
 fi
