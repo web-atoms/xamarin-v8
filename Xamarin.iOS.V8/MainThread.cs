f969fa1cd6920dd97f6a844f35a217d5fcc05076
Akash Kava <39438041+ackava@users.noreply.github.com>
2020-05-15 14:29:30 +0530

    Code moved to shared

---
+++ Xamarin.iOS.V8/MainThread.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Foundation;
+using ObjCRuntime;
+using UIKit;
+
+namespace NativeLibrary
+{
+    class Timeout : IDisposable { 
+    
+        public bool Disposed { get; set; }
+
+        void IDisposable.Dispose()
+        {
+            this.Disposed = true;
+        }
+    }
+
+    internal static partial class MainThread
+    {
+        static bool PlatformIsMainThread =>
+            NSThread.Current.IsMainThread;
+
+        static void PlatformBeginInvokeOnMainThread(Action action)
+        {
+            NSRunLoop.Main.BeginInvokeOnMainThread(action.Invoke);
+        }
+
+        static void PlatformBeginInvokeOnMainThread(Action action, TimeSpan ts)
+        {
+            NSRunLoop.Main.Invoke(action, ts);
+        }
+
+        public static IDisposable PostTimeout(Action action, long delayMillis)
+        {
+            var t = new Timeout();
+            PlatformBeginInvokeOnMainThread(() => { if (t.Disposed) action(); }, TimeSpan.FromMilliseconds(delayMillis));
+            return t;
+        }
+
+
+        public static bool IsMainThread =>
+            PlatformIsMainThread;
+
+        public static void BeginInvokeOnMainThread(Action action)
+        {
+            if (IsMainThread)
+            {
+                action();
+            }
+            else
+            {
+                PlatformBeginInvokeOnMainThread(action);
+            }
+        }
+
+        public static void BeginInvokeOnMainThread(Action action, long delayMillis)
+        {
+            PlatformBeginInvokeOnMainThread(action, TimeSpan.FromMilliseconds(delayMillis));
+        }
+
+        public static Task InvokeOnMainThreadAsync(Action action)
+        {
+            if (IsMainThread)
+            {
+                action();
+#if NETSTANDARD1_0
+                return Task.FromResult(true);
+#else
+                return Task.CompletedTask;
+#endif
+            }
+
+            var tcs = new TaskCompletionSource<bool>();
+
+            BeginInvokeOnMainThread(() =>
+            {
+                try
+                {
+                    action();
+                    tcs.TrySetResult(true);
+                }
+                catch (Exception ex)
+                {
+                    tcs.TrySetException(ex);
+                }
+            });
+
+            return tcs.Task;
+        }
+
+        public static Task<T> InvokeOnMainThreadAsync<T>(Func<T> func)
+        {
+            if (IsMainThread)
+            {
+                return Task.FromResult(func());
+            }
+
+            var tcs = new TaskCompletionSource<T>();
+
+            BeginInvokeOnMainThread(() =>
+            {
+                try
+                {
+                    var result = func();
+                    tcs.TrySetResult(result);
+                }
+                catch (Exception ex)
+                {
+                    tcs.TrySetException(ex);
+                }
+            });
+
+            return tcs.Task;
+        }
+
+        public static Task InvokeOnMainThreadAsync(Func<Task> funcTask)
+        {
+            if (IsMainThread)
+            {
+                return funcTask();
+            }
+
+            var tcs = new TaskCompletionSource<object>();
+
+            BeginInvokeOnMainThread(
+                async () =>
+                {
+                    try
+                    {
+                        await funcTask().ConfigureAwait(false);
+                        tcs.SetResult(null);
+                    }
+                    catch (Exception e)
+                    {
+                        tcs.SetException(e);
+                    }
+                });
+
+            return tcs.Task;
+        }
+
+        public static Task<T> InvokeOnMainThreadAsync<T>(Func<Task<T>> funcTask)
+        {
+            if (IsMainThread)
+            {
+                return funcTask();
+            }
+
+            var tcs = new TaskCompletionSource<T>();
+
+            BeginInvokeOnMainThread(
+                async () =>
+                {
+                    try
+                    {
+                        var ret = await funcTask().ConfigureAwait(false);
+                        tcs.SetResult(ret);
+                    }
+                    catch (Exception e)
+                    {
+                        tcs.SetException(e);
+                    }
+                });
+
+            return tcs.Task;
+        }
+
+        public static async Task<SynchronizationContext> GetMainThreadSynchronizationContextAsync()
+        {
+            SynchronizationContext ret = null;
+            await InvokeOnMainThreadAsync(() =>
+                ret = SynchronizationContext.Current).ConfigureAwait(false);
+            return ret;
+        }
+    }
+}
