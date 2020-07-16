0afe30fe90b8560951b356bc2177056cf0da8dd2
Akash Kava <39438041+ackava@users.noreply.github.com>
2020-07-16 14:30:22 +0530

    Not wrapped crash fixed

--- V8Sharp/JSValue.cs
+++ V8Sharp/JSValue.cs
@@ -181,6 +181,8 @@ namespace Xamarin.Android.V8
         {
             // we need to get wrapped instance..
             var w = this[jsContext.WrappedSymbol] as JSValue;
+            if (w.IsUndefined)
+                return (T)(object)null;
             IntPtr v = w.handle.result.refValue;
             //if (v == IntPtr.Zero)
             //{
